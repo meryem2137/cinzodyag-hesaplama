7bdf5fa3e33745c8ff69f2a4ee87d530d294e105
meryem2137 <116557318+meryem2137@users.noreply.github.com>
2023-01-06 22:19:39 +0300

    Add files via upload

---
+++ cinzodyagı.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class cinzodyagı {
+
+
+    public static void main(String[] args) {
+        int dogumyılı;
+    Scanner input=new Scanner(System.in);
+    System.out.print("doğum yılınızı giriniz: ");
+    dogumyılı=input.nextInt();
+
+    if(dogumyılı%12==0){
+        System.out.print("Çin zodyağı burcunuz: Maymun ");}
+
+       else if(dogumyılı%12==1){
+            System.out.print("Çin zodyağı burcunuz: Horoz ");}
+
+       else if(dogumyılı%12==2){
+            System.out.print("Çin zodyağı burcunuz: Köpek ");}
+
+        if(dogumyılı%12==3){
+            System.out.print("Çin zodyağı burcunuz: Domuz ");}
+
+       else if(dogumyılı%12==4){
+            System.out.print("Çin zodyağı burcunuz: Fare ");}
+
+      else  if(dogumyılı%12==5){
+            System.out.print("Çin zodyağı burcunuz: Öküz ");}
+
+
+      else  if(dogumyılı%12==6){
+            System.out.print("Çin zodyağı burcunuz: Kaplan ");}
+
+      else  if(dogumyılı%12==7){
+            System.out.print("Çin zodyağı burcunuz: Tavşan ");}
+
+     else   if(dogumyılı%12==8){
+            System.out.print("Çin zodyağı burcunuz: Ejderha ");}
+
+      else  if(dogumyılı%12==9){
+            System.out.print("Çin zodyağı burcunuz: Yılan ");}
+
+     else   if(dogumyılı%12==10){
+            System.out.print("Çin zodyağı burcunuz: At ");}
+
+
+    else{
+        System.out.print("Çin zodyağı burcunuz: Koyun");
+        }
+    }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
